eb3d23f2c9335355a256d4927af53c3497e43620
Joel <joel.espinoza@pucp.edu.pe>
2023-01-31 20:12:32 -0500

    Se ha creado el proyecto de SalesController y se ha añadido a la solucion de SalesSolution

---
+++ SalesController/SalesController.h
@@ -0,0 +1,13 @@
+#pragma once
+/* Clase que modela el componente controlador que se encarga
+* de gestionar los procesos del sistema. 
+*/
+
+using namespace System;
+
+namespace SalesController {
+	public ref class Class1
+	{
+		// TODO: Agregue aquí los métodos de esta clase.
+	};
+}
